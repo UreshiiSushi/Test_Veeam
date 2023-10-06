01af0a43b4a71c2d660df4bdde1c8fb47df475a9
UreshiiSushi <merieru@gmail.com>
2023-10-06 17:23:21 +0200

    init and added func for copy files

---
+++ sync_folders.py
@@ -0,0 +1,73 @@
+import sys
+import schedule
+import shutil
+import time
+import os
+from pathlib import Path
+from datetime import timedelta
+from distutils import file_util, log
+
+
+def sync_folders(source: Path, copy: Path):
+    if not copy.exists():
+        copy.mkdir()
+    for i in source.glob("**\*"):
+        if i.is_file():
+            log.set_verbosity(log.DEBUG)
+            file_util.copy_file(i, copy, update=1)
+    for j in copy.glob("**\*"):
+        if j.is_file():
+            if not (j in source.glob("**\*")):
+                log.set_verbosity(log.DEBUG)
+                os.remove(j)
+            
+
+    # check existing of copy folder
+    # if not : make new folder and copy all files from source
+    # if yes : check files
+
+
+# def set_sync_period(period: timedelta, source: Path, copy: Path):
+#     schedule.every(period).seconds.do(main)
+
+
+def main():
+    try:
+        source_path = Path(sys.argv[1])
+    except IndexError:
+        return "No source path entered"
+
+    try:
+        copy_path = Path(sys.argv[2])
+    except IndexError:
+        return "No copy path entered"
+
+    try:
+        # period = timedelta(minutes=float(sys.argv[3]))
+        period = int(sys.argv[3])
+    except IndexError:
+        print("No time period entered")
+    except ValueError:
+        print("Period is not a number")
+
+    if not source_path.exists():
+        print("Source path does not exists")
+
+    sync_folders(source_path, copy_path)
+    
+    # set_sync_period(period, source_path, copy_path)
+
+    print("All ok")
+
+
+if __name__ == "__main__":
+    run_counter = 0
+    result = main()
+    print(result)
+    # schedule.every(int(sys.argv[3])).seconds.do(main)
+    # while True:
+    #     schedule.run_pending()
+    #     run_counter += 1
+    #     if run_counter == 9:
+    #         break
+    #     time.sleep(1)
